524346f2f79517b5af4d91de4550feff55f3e13c
Collin <c22guo@uwaterloo.ca>
2024-01-17 21:17:41 -0500

    deployed

--- function_app.py
+++ function_app.py
@@ -2,12 +2,12 @@ import logging
 import time
 import azure.functions as func
 import pandas_market_calendars as mcal
-from datetime import datetime
+import datetime
 from bot import Bot
 from consts import *
 app = func.FunctionApp()
 
-@app.schedule(schedule="* 30 10 * * WED", arg_name="myTimer", run_on_startup=True,
+@app.schedule(schedule="* 30 10,12 * * WED", arg_name="myTimer", run_on_startup=True,
               use_monitor=False) 
 def testing(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
@@ -15,17 +15,19 @@ def testing(myTimer: func.TimerRequest) -> None:
 
     logging.info('Python timer trigger function executed.')
 
+    utc_now = datetime.datetime.now()
+
     #implement logic
     is_open = market_is_open(datetime.now().strftime("%Y-%m-%d"))
     logging.info("got dates")
-    while not is_open:
-        time.sleep(24*60*60)
-    logging.info("waited until open")
-    Bot(API).close_all()
-    logging.info("sold stuff")
-    time.sleep(2*60*60)
-    Bot(API).make_buys()
-    logging.info("bought stuff")
+    if not is_open:
+        return
+    if utc_now.hour == 10:
+        Bot(API).close_all()
+        logging.info("sold stuff")
+    elif utc_now.hour == 12:    
+        Bot(API).make_buys()
+        logging.info("bought stuff")
 
 
 
